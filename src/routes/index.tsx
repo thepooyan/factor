0026bbb0a93ffcb61b7f4d64fbdc6be68a06fea7
pooyan <thepooyan@gmail.com>
2025-04-03 22:06:35 +0330

    nothing!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,16 @@ import { APP_NAME } from "~/utility/settings";
 
 export default function Home() {
 
+  const google = () => {
+    window.location.href = "http://localhost:8000/auth/google";
+  }
+
   return (
     <div class="flex flex-col gap-1 p-2">
       <h1 class="text-center text-2xl mb-4"> به {APP_NAME} خوش آمدید! </h1>
       <Button as={A} href="/Signup">ثبت نام</Button>
       <Button as={A} href="/Login">لاگین</Button>
+      <Button onclick={google}>Continue with Google</Button>
     </div>
   );
 }
