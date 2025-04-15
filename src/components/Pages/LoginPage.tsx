d3163bdcd40c52ca90a5fcf046106a02fcb83738
pooyan <thepooyan@gmail.com>
2025-04-15 16:53:59 +0330

    nothing!

--- src/components/Pages/LoginPage.tsx
+++ src/components/Pages/LoginPage.tsx
@@ -2,6 +2,7 @@ import { Tabs, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { AiOutlineUser } from "solid-icons/ai";
 import LoginTab from "./LoginTab";
 import SignupTab from "./SignupTab";
+import { A } from "@solidjs/router";
 
 export default function LoginPage({activatedTab}:{activatedTab: "login" | "signup"}) {
   return (
@@ -10,12 +11,12 @@ export default function LoginPage({activatedTab}:{activatedTab: "login" | "signu
       dir="rtl"
     >
       <div class="w-full max-w-md">
-        <div class="mb-6 text-center">
+        <A href="/" class="mb-6 text-center block">
           <div class="inline-block p-3 rounded-full bg-gradient-to-bl shadow-lg from-zinc-400 via-zinc-500 to-zinc-600  mb-2">
             <AiOutlineUser class="h-8 w-8 text-white" />
           </div>
           <h1 class="text-2xl font-bold text-gray-800">سامانه کاربری</h1>
-        </div>
+        </A>
         <Tabs value={activatedTab} class="w-full">
           <TabsList class="grid w-full grid-cols-2 p-1">
             <TabsTrigger
