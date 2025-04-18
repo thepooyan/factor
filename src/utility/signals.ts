6cc7eaf8ef8c5800a57bf9434445421cc2813a36
pooyan <thepooyan@gmail.com>
2025-04-18 17:06:31 +0330

    nothing!

--- src/utility/signals.ts
+++ src/utility/signals.ts
@@ -1,12 +1,12 @@
 import { createSignal } from "solid-js";
 import { Iuser } from "./interface";
-import { deleteCookie, getCookie, setCookie } from "vinxi/http";
+// import { deleteCookie, getCookie, setCookie } from "vinxi/http";
 
 const getInitialUser = () => {
-  let userCookie = getCookie("auth")
-  if (userCookie) {
-    return JSON.parse(userCookie) as Iuser
-  }
+  // let userCookie = getCookie("auth")
+  // if (userCookie) {
+  //   return JSON.parse(userCookie) as Iuser
+  // }
   return null
 }
 
@@ -15,11 +15,11 @@ const [userSignal, setUserSignal] = createSignal<Iuser | null>(getInitialUser())
 export const userMg = {
   login: (user: Iuser) => {
     setUserSignal(user)
-    setCookie("auth", JSON.stringify(user))
+    // setCookie("auth", JSON.stringify(user))
   },
   logout: () => {
     setUserSignal(null)
-    deleteCookie("auth")
+    // deleteCookie("auth")
   },
   get: () => {
     return userSignal()
