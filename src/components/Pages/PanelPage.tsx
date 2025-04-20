5d71d9ae0cd64a67a9446817668dfcff57aa2a5c
pooyan <thepooyan@gmail.com>
2025-04-20 23:53:30 +0330

    panel page update

--- src/components/Pages/PanelPage.tsx
+++ src/components/Pages/PanelPage.tsx
@@ -1,11 +1,13 @@
 import { FiCreditCard, FiHelpCircle, FiHome, FiInfo, FiMessageSquare, FiSettings, FiUser } from "solid-icons/fi"
 import { createSignal } from "solid-js"
 import { Tabs, TabsList, TabsTrigger } from "../ui/tabs"
+import { IconTypes } from "solid-icons"
 
 export function PanelPage() {
-  const [activeTab, setActiveTab] = createSignal("dashboard")
+  const [activeTab, setActiveTab] = createSignal<tabsType>("dashboard")
 
-  const tabs = [
+  type tabsType = "dashboard" | "profile" | "settings" | "customers" | "factor" | "payments" | "support"
+  const tabs: {id: tabsType, label: string, icon: IconTypes}[] = [
     { id: "dashboard", label: "داشبورد", icon: FiHome },
     { id: "profile", label: "پروفایل", icon: FiUser },
     { id: "settings", label: "تنظیمات", icon: FiSettings },
@@ -17,11 +19,9 @@ export function PanelPage() {
 
   return (
     <div class="mx-auto max-w-7xl p-5" dir="rtl">
-      <h1 class="mb-8 text-3xl font-bold text-gray-900">پنل کاربری</h1>
-
       <div class="grid grid-cols-1 gap-6 md:grid-cols-[250px_1fr]">
         <div class="order-2 md:order-1">
-          <Tabs orientation="vertical" value={activeTab()} onvolumechange={setActiveTab} class="w-full">
+          <Tabs orientation="vertical" class="w-full">
             <TabsList class="flex h-auto w-full flex-col justify-start bg-white p-0 shadow-md">
               {tabs.map((tab) => {
                 const Icon = tab.icon
@@ -29,6 +29,7 @@ export function PanelPage() {
                   <TabsTrigger
                     value={tab.id}
                     class="flex w-full justify-start gap-2 border-b border-gray-100 px-4 py-3 text-right data-[state=active]:bg-gray-50"
+                    onClick={() => setActiveTab(tab.id)}
                   >
                     <Icon class="h-5 w-5" />
                     <span>{tab.label}</span>
@@ -43,8 +44,8 @@ export function PanelPage() {
           {activeTab() === "dashboard" && <>dash</>}
           {activeTab() === "profile" && <>pro</>}
           {activeTab() === "settings" && <>set</>}
-          {activeTab() === "messages" && <>me</>}
-          {activeTab() === "orders" && <></>}
+          {activeTab() === "customers" && <></>}
+          {activeTab() === "factor" && <>me</>}
           {activeTab() === "payments" && <></>}
           {activeTab() === "support" && <></>}
         </div>
