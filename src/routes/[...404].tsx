9550adc9ef7c81efbbe1c33f1636401148730039
pooyan <thepooyan@gmail.com>
2025-04-24 17:12:12 +0330

    nothing!

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
-    <div class="w-full flex justify-center items-center h-dvh">
-      Page not Found
+    <div class="w-full flex flex-col justify-center items-center gap-5 h-dvh text-xl font-bold">
+      صفحه مورد نظر یافت نشد
     </div>
   );
 }
