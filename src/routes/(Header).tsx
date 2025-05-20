173ca6eaac66f3e29546c12e722cb51fec7d6970
pooyan <thepooyan@gmail.com>
2025-05-20 15:32:53 +0330

    nothing!

--- src/routes/(Header).tsx
+++ src/routes/(Header).tsx
@@ -1,6 +1,7 @@
 import { RouteSectionProps } from "@solidjs/router"
 import Footer from "~/components/Layout/Footer"
 import Header from "~/components/Layout/Header"
+import "../app.scss"
 
 const HeaderGroup = (props: RouteSectionProps) => {
   return (
