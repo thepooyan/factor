3996aad71d4d688cf4060960014924b95df36b41
pooyan <thepooyan@gmail.com>
2025-05-21 17:00:53 +0330

    nothing!

---
+++ src/routes/(Header)/NewCompany.tsx
@@ -0,0 +1,11 @@
+import Company from "~/components/Pages/Company"
+
+const NewCompany = () => {
+  return (
+    <div class="p-4">
+      <Company isNew/>
+    </div>
+  )
+}
+
+export default NewCompany
