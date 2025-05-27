4cf9f8fec769d5fb3d5628493822a61ca4f426b1
pooyan <thepooyan@gmail.com>
2025-05-27 15:46:48 +0330

    nothing!

--- src/components/general/Input.tsx
+++ src/components/general/Input.tsx
@@ -2,18 +2,19 @@ import { cn } from "~/lib/utils";
 import {JSX} from "solid-js"
 
 interface InputProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
-  "data-validate"?: string
+  validate?: string
   noErrorEmit?: boolean
   errorClass?: string
 }
 const Input = (props:InputProps) => {
 
   const ec = props.errorClass ? {'data-errorclass': props.errorClass} : {}
+  const v = props.validate ? {'data-validate': props.validate} : {}
 
   return (
     <>
-      <input {...props} {...ec} class={cn("p-2 w-full border-1 border-zinc-300 rounded-md bg-transparent min-h-10 " + props.class)}/>
-      {(props["data-validate"]) && !props.noErrorEmit && <div class="validation-error text-red-500 text-sm pr-2 mt-1"></div>}
+      <input {...props} {...ec} {...v} class={cn("p-2 w-full border-1 border-zinc-300 rounded-md bg-transparent min-h-10 " + props.class)}/>
+      {props.validate && !props.noErrorEmit && <div class="validation-error text-red-500 text-sm pr-2 mt-1"></div>}
     </>
   );
 };
