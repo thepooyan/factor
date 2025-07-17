ebb90aeda8a8f225f2b3538b12e72decf381992e
pooyan <thepooyan@gmail.com>
2025-07-17 17:48:49 +0330

    nothing!

--- src/routes/(css)/(Header)/Panel/(Panel).tsx
+++ src/routes/(css)/(Header)/Panel/(Panel).tsx
@@ -1,8 +1,12 @@
+import { Show } from "solid-js"
 import Company from "~/components/Pages/Company"
+import { selectedCompany } from "~/utility/signals"
 
 const Panel = () => {
   return (
-    <Company/>
+    <Show when={selectedCompany()}>
+      {a => <Company initialData={a}/>}
+    </Show>
   )
 }
 
