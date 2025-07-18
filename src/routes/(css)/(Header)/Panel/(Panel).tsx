9fa47e80ebca9940d002825c5efa163936476b37
pooyan <thepooyan@gmail.com>
2025-07-18 23:28:20 +0330

    nothing!

--- src/routes/(css)/(Header)/Panel/(Panel).tsx
+++ src/routes/(css)/(Header)/Panel/(Panel).tsx
@@ -1,14 +1,22 @@
-import { Show } from "solid-js"
+import { UseQueryResult } from "@tanstack/solid-query"
+import { AxiosResponse } from "axios"
+import { onMount, Show } from "solid-js"
 import Company from "~/components/Pages/Company"
+import { ICompany } from "~/utility/interface"
 import { queryCompanies } from "~/utility/queries"
 import { selectedCompany } from "~/utility/signals"
 
 const Panel = () => {
 
+  let query:UseQueryResult<AxiosResponse<ICompany[]>>
+  onMount(() => {
+    query = queryCompanies()
+  })
+
   const folan = () => {
     const selected = selectedCompany()?.company_id;
-    if (!selected) return null
-    return queryCompanies().data?.data.find(a => a.company_id ===  selected) || null
+    if (!selected || !query) return null
+    return query.data?.data.find(a => a.company_id ===  selected) || null
   }
 
   return (
