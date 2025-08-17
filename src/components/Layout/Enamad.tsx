63dce72c48138861d49093f005e64fa2834693c7
pooyan <thepooyan@gmail.com>
2025-08-17 11:38:02 +0330

    update enamad

--- src/components/Layout/Enamad.tsx
+++ src/components/Layout/Enamad.tsx
@@ -1,13 +1,35 @@
 const Enamad = () => {
   return (
     <>
-      <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=636752&Code=JqYklx7nQVPEzZmZUwOANk4kfdBAgi0z'>
-      <img referrerpolicy='origin'
-      src='https://trustseal.enamad.ir/logo.aspx?id=636752&Code=JqYklx7nQVPEzZmZUwOANk4kfdBAgi0z' alt=''
-      style='cursor:pointer' data-code='JqYklx7nQVPEzZmZUwOANk4kfdBAgi0z'/>
+      <a
+        referrerpolicy="origin"
+        target="_blank"
+        href="https://trustseal.enamad.ir/?id=636752&Code=JqYklx7nQVPEzZmZUwOANk4kfdBAgi0z"
+      >
+        <img
+          referrerpolicy="origin"
+          src="https://trustseal.enamad.ir/logo.aspx?id=636752&Code=JqYklx7nQVPEzZmZUwOANk4kfdBAgi0z"
+          alt=""
+          style="cursor:pointer"
+          data-code="JqYklx7nQVPEzZmZUwOANk4kfdBAgi0z"
+        />
       </a>
+      <img
+        referrerPolicy="origin"
+        id="rgvjsizpnbqejxlzjzpeoeuk"
+        style={{ cursor: "pointer" }}
+        onClick={() =>
+          window.open(
+            "https://logo.samandehi.ir/Verify.aspx?id=392178&p=xlaopfvluiwkrfthjyoemcsi",
+            "Popup",
+            "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30",
+          )
+        }
+        alt="logo-samandehi"
+        src="https://logo.samandehi.ir/logo.aspx?id=392178&p=qftibsiyodrfnbpdyndtaqgw"
+      />
     </>
-  )
-}
+  );
+};
 
-export default Enamad
+export default Enamad;
