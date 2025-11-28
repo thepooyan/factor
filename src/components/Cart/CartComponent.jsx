f29c739e38065721a25adfc996ae79aaa337b9dc
Mehtar21 <mha13777@gmail.com>
2025-11-28 22:13:54 +0330

    edited cart icon

--- src/components/Cart/CartComponent.jsx
+++ src/components/Cart/CartComponent.jsx
@@ -1,26 +1,21 @@
 import {  useCart } from '~/context/Cart/cartContext';
 import { Show } from 'solid-js';
 import { CartModal } from './CartModal';
+import { FiShoppingCart  } from 'solid-icons/fa'
 
 
 export function CartComponent() { 
     const { cartItemCount, toggleCart, isCartOpen } = useCart();
 
     return (
-    <header style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      padding: '15px 30px',
-      background: 'red',
-      color: 'white'
-    }}>
       <div 
         style={{position: 'relative', cursor: 'pointer'}}
         onClick={toggleCart} // اینجا سبد باز میشه
       >
 
         
-        🛒
+        <FiShoppingCart class="h-6 w-6" /> 
+      
         <span class="absolute top-[-8px] right-[-18px] bg-blue-600 text-white rounded-full text-xs px-1.5 py-0.5 leading-none">
           {cartItemCount()}
         </span>
@@ -29,6 +24,5 @@ export function CartComponent() {
         </Show>
 
       </div>
-    </header>
   );
 }
